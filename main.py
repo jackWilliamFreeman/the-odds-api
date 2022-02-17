ac05f3ddf11a00d62d11ad4123829342e5b40c72
William Freeman <wfreeman@fmgl.com.au>
2022-02-17 21:58:20 +0800

    fixed other methods

--- main.py
+++ main.py
@@ -2,19 +2,13 @@ from audioop import avg
 from turtle import home
 import requests
 import os
+from methods import get_odds_for_sessions, determine_winner
 
-# An api key is emailed to you when you sign up to a plan
-# Get a free API key at https://api.the-odds-api.com/
 API_KEY = os.getenv('KEY')
-
-SPORT = 'upcoming' # use the sport_key from the /sports endpoint below, or use 'upcoming' to see the next 8 games across all sports
-
-REGIONS = 'au' # uk | us | eu | au. Multiple can be specified if comma delimited
-
+SPORT = 'upcoming'
+REGIONS = 'au' 
 MARKETS = 'h2h' # h2h | spreads | totals. Multiple can be specified if comma delimited
-
-ODDS_FORMAT = 'decimal' # decimal | american
-
+ODDS_FORMAT = 'decimal'
 DATE_FORMAT = 'iso' # iso | unix
 
 class game_odds:
@@ -26,17 +20,7 @@ class game_odds:
         self.away_odds={} 
         self.home_avg_odds = 0
         self.away_avg_odds = 0
-        self.predicted_winner = ""
-
-
-    
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-#
-# First get a list of in-season sports
-#   The sport 'key' from the response can be used to get odds in the next request
-#
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+        self.predicted_winner = ""    
 
 sports_response = requests.get(
     'https://api.the-odds-api.com/v4/sports', 
@@ -45,13 +29,11 @@ sports_response = requests.get(
     }
 )
 
-
 if sports_response.status_code != 200:
     print(f'Failed to get sports: status_code {sports_response.status_code}, response body {sports_response.text}')
 
 else:
     print('List of in season sports:', sports_response.json())
-
 
 global afl_obj
 for sport in sports_response.json():
@@ -60,15 +42,6 @@ for sport in sports_response.json():
         SPORT = afl_obj['key']
 
 print(afl_obj)
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-#
-# Now get a list of live & upcoming games for the sport you want, along with odds for different bookmakers
-# This will deduct from the usage quota
-# The usage quota cost = [number of markets specified] x [number of regions specified]
-# For examples of usage quota costs, see https://the-odds-api.com/liveapi/guides/v4/#usage-quota-costs
-#
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 odds_response = requests.get(
     f'https://api.the-odds-api.com/v4/sports/{SPORT}/odds',
@@ -80,36 +53,6 @@ odds_response = requests.get(
         'dateFormat': DATE_FORMAT,
     }
 )
-
-def get_odds_for_sessions(game_odds, odds_json):
-    games = []
-    for game in odds_json:        
-        home_team = game['home_team']
-        away_team = game['away_team']
-        game_date = game['commence_time']
-        game_odds_collection = game_odds(home_team, away_team, game_date)
-        for bookkeeper in game['bookmakers']:
-            outcomes = bookkeeper['markets'][0]['outcomes']
-            for outcome in outcomes:
-                if outcome['name'] == home_team:
-                    game_odds_collection.home_odds[bookkeeper['title']] = outcome['price']
-                else:
-                    game_odds_collection.away_odds[bookkeeper['title']] = outcome['price']
-        games.append(game_odds_collection)
-    return games
-
-def determine_winner(games):
-    for game in games:
-        away_odds = game.away_odds.values()
-        home_odds = game.home_odds.values()
-        game.away_avg_odds = sum(away_odds)/len(away_odds)
-        game.home_avg_odds = sum(home_odds)/len(home_odds)
-
-        if game.away_avg_odds > game.home_avg_odds:
-            game.predicted_winner = game.away_team
-        else:
-            game.predicted_winner = game.home_team
-    return games
 
 if odds_response.status_code != 200:
     print(f'Failed to get odds: status_code {odds_response.status_code}, response body {odds_response.text}')
@@ -125,3 +68,4 @@ else:
     # Check the usage quota
     print('Remaining requests', odds_response.headers['x-requests-remaining'])
     print('Used requests', odds_response.headers['x-requests-used'])
+
---
+++ methods.py
@@ -0,0 +1,29 @@
+def get_odds_for_sessions(game_odds, odds_json):
+    games = []
+    for game in odds_json:        
+        home_team = game['home_team']
+        away_team = game['away_team']
+        game_date = game['commence_time']
+        game_odds_collection = game_odds(home_team, away_team, game_date)
+        for bookkeeper in game['bookmakers']:
+            outcomes = bookkeeper['markets'][0]['outcomes']
+            for outcome in outcomes:
+                if outcome['name'] == home_team:
+                    game_odds_collection.home_odds[bookkeeper['title']] = outcome['price']
+                else:
+                    game_odds_collection.away_odds[bookkeeper['title']] = outcome['price']
+        games.append(game_odds_collection)
+    return games
+
+def determine_winner(games):
+    for game in games:
+        away_odds = game.away_odds.values()
+        home_odds = game.home_odds.values()
+        game.away_avg_odds = sum(away_odds)/len(away_odds)
+        game.home_avg_odds = sum(home_odds)/len(home_odds)
+
+        if game.away_avg_odds > game.home_avg_odds:
+            game.predicted_winner = game.away_team
+        else:
+            game.predicted_winner = game.home_team
+    return games
